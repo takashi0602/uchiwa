022faebc95cf27dd8db0bdcc3c6760fb0f8e02b4
takashi <takashioicsn@gmail.com>
2018-08-24 21:00:16 +0900

    Update

--- uchiwa_angle.py
+++ uchiwa_angle.py
@@ -19,11 +19,11 @@ servo = GPIO.PWM(gp_out, 50)
 servo.start(0)
 
 # デューティサイクルの値を変更することでサーボが回って角度が変わる。
-servo.ChangeDutyCycle(0.1)
+servo.ChangeDutyCycle(2)
 time.sleep(0.5)
 
-servo.ChangeDutyCycle(6.1)
+servo.ChangeDutyCycle(7)
 time.sleep(0.5)
 
 servo.stop()
-GPIO.cleanup() 
+GPIO.cleanup()
