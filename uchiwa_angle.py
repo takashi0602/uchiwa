c122e8c8682ff56ea809f7413841f5520c633979
Kazuki Tobita <kazukiti201@gmail.com>
2018-08-26 16:02:53 +0900

    Refactoring

--- uchiwa_angle.py
+++ uchiwa_angle.py
@@ -3,25 +3,29 @@
 import RPi.GPIO as GPIO
 import time
 
-# GPIOのモード設定
-GPIO.setmode(GPIO.BCM)
+def main():
+    # GPIOのモード設定
+    GPIO.setmode(GPIO.BCM)
 
-# GPIO18を制御パルスの出力に設定
-gp_out = 18
-GPIO.setup(gp_out, GPIO.OUT)
+    # GPIO18を制御パルスの出力に設定
+    gp_out = 18
+    GPIO.setup(gp_out, GPIO.OUT)
 
-# サーボの制御パルスと周波数の設定
-servo = GPIO.PWM(gp_out, 50)
+    # サーボの制御パルスと周波数の設定
+    servo = GPIO.PWM(gp_out, 50)
 
-# パルス出力の開始
-servo.start(0)
+    # パルス出力の開始
+    servo.start(0)
 
-# サーボを動作させる
-servo.ChangeDutyCycle(2)
-time.sleep(0.5)
-servo.ChangeDutyCycle(7)
-time.sleep(0.5)
+    # サーボを動作させる
+    servo.ChangeDutyCycle(2)
+    time.sleep(0.5)
+    servo.ChangeDutyCycle(7)
+    time.sleep(0.5)
 
-# 後処理
-servo.stop()
-GPIO.cleanup()
+    # 後処理
+    servo.stop()
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    main()
--- uchiwa_direction.py
+++ uchiwa_direction.py
@@ -3,40 +3,48 @@
 import RPi.GPIO as GPIO
 import time
 
-# GPIOのモード設定
-GPIO.setmode(GPIO.BCM)
+def main():
+    # GPIOのモード設定
+    GPIO.setmode(GPIO.BCM)
 
-# GPIO17を制御パルスの出力に設定
-gp_out = 17
-GPIO.setup(gp_out, GPIO.OUT)
+    # GPIO17を制御パルスの出力に設定
+    gp_out = 17
+    GPIO.setup(gp_out, GPIO.OUT)
 
-# サーボの制御パルスと周波数の設定
-servo = GPIO.PWM(gp_out, 50)
+    # サーボの制御パルスと周波数の設定
+    servo = GPIO.PWM(gp_out, 50)
 
-# パルス出力の開始
-servo.start(0)
+    # パルス出力の開始
+    servo.start(0)
 
-# ファイル読み込み
-fr = open('direction', 'r')
+    # 現在の角度を取得
+    direction = read_direction()
 
-# 現在の角度を取得
-direction = 0
-for row in fr:
-    direction = int(row.strip())
-fr.close()
+    # サーボを動作させる
+    servo.ChangeDutyCycle(direction)
+    time.sleep(0.5)
 
-# サーボを動作させる
-servo.ChangeDutyCycle(direction)
-time.sleep(0.5)
+    # 角度を書き込み
+    if direction >= 12:
+        direction = 2
+    write_direction(direction)
 
-# ファイル書き込み
-if direction >= 12:
-    direction = 2
+    # 後処理
+    servo.stop()
+    GPIO.cleanup()
 
-fw = open('direction', 'w')
-fw.write(str(direction + 1))
-fw.close()
+def read_direction():
+    fr = open('direction', 'r')
+    direction = 0
+    for row in fr:
+        direction = int(row.strip())
+    fr.close()
+    return direction
 
-# 後処理
-servo.stop()
-GPIO.cleanup()
+def write_direction(direction):
+    fw = open('direction', 'w')
+    fw.write(str(direction + 1))
+    fw.close()
+
+if __name__ == '__main__':
+    main()
